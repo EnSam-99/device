b398624dc393d4f6356149b9e6de5d8531bc299a
Enoq Samsonyan <enoq.samsonyan@imaginelive.com>
2025-12-14 05:19:15 +0400

    fixes made

--- LiveCasino.TableController/Program.cs
+++ LiveCasino.TableController/Program.cs
@@ -1,5 +1,6 @@
 using System.Net.Http;
 using Grpc.Core;
+using LiveCasino.TableController.Components;
 using LiveCasino.TableController.Models;
 using LiveCasino.TableController.Protos;
 using LiveCasino.TableController.Services;
